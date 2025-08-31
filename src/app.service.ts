90209bafcded5896ee40525307e0f5090d977969
Андрій Гушуватий <88794434+hushuvatyi@users.noreply.github.com>
2025-08-31 23:59:56 +0300

    com 3

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! I did it!';
+    return 'Hello World! Дивно. Тут працює.';
   }
 }
